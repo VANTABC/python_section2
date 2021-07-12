cc17071cdb4de97c318b922d7456ad01183c3982
SeungMin_Hyun <blackcoding911@gmail.com>
2021-07-12 12:59:12 +0900

    두 번째 수정 v1.1

--- download2-2-1.py
+++ download2-2-1.py
@@ -7,7 +7,7 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 imgUrl = "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2F4b%2F09%2F62%2F4b09628c4e24e141eb2f7d0c2b036d52--cute-cats-big-cats.jpg&type=a340"
 htmlUrl = "http://google.com"
-
+print('Hi')
 savePath1 = "c:/python/section2/test1.jpg"
 savePath2 = "c:/python/section2/index.html"
 
